6a5572aed83ee005f2c5312066575f270b6849f4
Svetlana <54301223+scherepa@users.noreply.github.com>
2023-04-30 20:43:43 +0300

    Add files via upload

---
+++ db/mongoConnect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/cards_project", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("mongo connected to cards_project");
+  // we're connected!
+});
+
+module.exports = db;
