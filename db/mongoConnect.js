ddc17c3da496f13b6f62efd175ad6cd2f19ffc64
Svetlana <54301223+scherepa@users.noreply.github.com>
2023-05-25 00:50:34 +0300

    Update mongoConnect.js

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/cards_project", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI||"mongodb://0.0.0.0:27017/cards_project", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
